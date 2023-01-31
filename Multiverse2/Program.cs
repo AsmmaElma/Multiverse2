b4b9ad89f41e174622a767892d5c3238f9f1aa24
macbookair <macbookair@Airdemacbookair.lan>
2023-01-31 10:38:12 +0100

    first commit

---
+++ Multiverse2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Multiverse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Main!");
+        }
+    }
+}
+
